c0adf052a046df2a1e4d4cd5b534a286d77db729
Daniel <daniellimas2000@gmail.com>
2022-12-18 16:40:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Agente_Coordenador(Agent):
             
             ########################################################
             # Codigo para enviar a equação pra cada um dos agentes #
-            # if (operador x) -> envie para agente x               # 
+            # if (operador x) -> envie para agente x               #
             ########################################################
             
             # Removendo os operadores para enviar apenas os numeros
@@ -84,6 +84,102 @@ class Agente_Subtrator(Agent):
 
     async def setup(self):
         self.add_behaviour(self.ReceiveMsg())
+        
+# Multiplicador
+class Agente_Multiplicador(Agent):
+    class ReceiveMsg(CyclicBehaviour):
+        async def on_start(self):
+            print("Ativando Agente Multiplicador")
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                operando = msg.body.split(" ") # transformando a string num array
+                print(f"Agente Multiplicador: Mensagem Recebida, realizando operação {operando[0]} * {operando[1]}")
+                resultado = float(operando[0]) * float(operando[1])
+                print(f"Agente Multiplicador: Calculo realizado, o resultado é {resultado}")
+                
+                # Codigo para enviar o resultado do calculo de volta
+                # sender = msg.sender
+                # msg = Message(to=str(sender))
+                # msg.body = str(resultado)
+                # await self.send(msg)
+                # print(f'Agente Somador: Resposta enviada')
+                # time.sleep(5)
+
+    async def setup(self):
+        self.add_behaviour(self.ReceiveMsg())
+        
+# Divisor
+class Agente_Divisor(Agent):
+    class ReceiveMsg(CyclicBehaviour):
+        async def on_start(self):
+            print("Ativando Agente Divisor")
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                operando = msg.body.split(" ") # transformando a string num array
+                print(f"Agente Divisor: Mensagem Recebida, realizando operação {operando[0]} / {operando[1]}")
+                resultado = float(operando[0]) / float(operando[1])
+                print(f"Agente Divisor: Calculo realizado, o resultado é {resultado}")
+                
+                # Codigo para enviar o resultado do calculo de volta
+                # sender = msg.sender
+                # msg = Message(to=str(sender))
+                # msg.body = str(resultado)
+                # await self.send(msg)
+                # print(f'Agente Somador: Resposta enviada')
+                # time.sleep(5)
+
+    async def setup(self):
+        self.add_behaviour(self.ReceiveMsg())
+        
+# Potencia
+class Agente_Potencia(Agent):
+    class ReceiveMsg(CyclicBehaviour):
+        async def on_start(self):
+            print("Ativando Agente Potencia")
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                operando = msg.body.split(" ") # transformando a string num array
+                print(f"Agente Potencia: Mensagem Recebida, realizando operação {operando[0]} ^ {operando[1]}")
+                resultado = float(operando[0]) ** float(operando[1])
+                print(f"Agente Potencia: Calculo realizado, o resultado é {resultado}")
+                
+                # Codigo para enviar o resultado do calculo de volta
+                # sender = msg.sender
+                # msg = Message(to=str(sender))
+                # msg.body = str(resultado)
+                # await self.send(msg)
+                # print(f'Agente Somador: Resposta enviada')
+                # time.sleep(5)
+
+    async def setup(self):
+        self.add_behaviour(self.ReceiveMsg())
+    
+# Raiz Quadrada
+class Agente_Raiz_Quadrada(Agent):
+    class ReceiveMsg(CyclicBehaviour):
+        async def on_start(self):
+            print("Ativando Agente Raiz Quadrada")
+        async def run(self):
+            msg = await self.receive()
+            if msg:
+                operando = msg.body.split(" ") # transformando a string num array
+                print(f"Agente Raiz Quadrada: Mensagem Recebida, realizando operação √{operando[0]}") # o simbolo da radiciação é #
+                resultado = float(operando[0]) ** (1/2)
+                print(f"Agente Raiz Quadrada: Calculo realizado, o resultado é {resultado}")
+                
+                # Codigo para enviar o resultado do calculo de volta
+                # sender = msg.sender
+                # msg = Message(to=str(sender))
+                # msg.body = str(resultado)
+                # await self.send(msg)
+                # print(f'Agente Somador: Resposta enviada')
+                # time.sleep(5)
+
+    async def setup(self):
+        self.add_behaviour(self.ReceiveMsg())
 
 # Main
 if __name__ == "__main__":
@@ -93,6 +189,22 @@ if __name__ == "__main__":
     future.result()
     
     subtrator = Agente_Subtrator("subtrator@127.0.0.1", "spade2000")
+    future = subtrator.start()
+    future.result()
+    
+    multiplicador = Agente_Multiplicador("multiplicador@127.0.0.1", "spade2000")
+    future = subtrator.start()
+    future.result()
+    
+    divisor = Agente_Divisor("divisor@127.0.0.1", "spade2000")
+    future = subtrator.start()
+    future.result()
+    
+    potencia = Agente_Potencia("potencia@127.0.0.1", "spade2000")
+    future = subtrator.start()
+    future.result()
+    
+    raiz_quadrada = Agente_Raiz_Quadrada("raiz_quadrada@127.0.0.1", "spade2000")
     future = subtrator.start()
     future.result()
     
